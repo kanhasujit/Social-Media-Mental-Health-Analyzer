2cd1f7c755e077abca718d206a8fbbc554de7f38
kanhasujit <151814173+kanhasujit@users.noreply.github.com>
2026-04-18 15:58:08 +0530

    updated The app icon

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -6,9 +6,11 @@ import sqlite3
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit.components.v1 as components
+from PIL import Image
 
 st.set_page_config(
     page_title="Mental Health & Social Media Analyzer",
+    page_icon=Image.open("../app/mental_health_logo.png"),
     layout="wide"
 )
 
